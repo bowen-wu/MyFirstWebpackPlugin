15d20fa2c95698edad0516bd26454c90325e6a47
bowen-wu <13793248162@163.com>
2022-10-13 23:05:00 +0800

    feat: change to class

--- src/my-first-webpack-plugin.ts
+++ src/my-first-webpack-plugin.ts
@@ -1,20 +1,23 @@
-function MyFirstWebpackPlugin(options) {
+// A JavaScript class.
+class MyFirstWebpackPlugin {
+    // Define `apply` as its prototype method which is supplied with compiler as its argument
+    apply(compiler) {
+        // 指定一个挂载到 webpack 自身的事件钩子。
+        // 设置回调来访问 compilation 对象：
+        // compilation对象 => 处理 webpack 内部实例的特定数据。
+        compiler.hooks.emit.tapAsync(
+            'MyFirstWebpackPlugin',
+            (compilation, callback) => {
+                console.log('This is my first webpack plugin!');
+                console.log(
+                    'Here’s the `compilation` object which represents a single build of assets:',
+                    compilation
+                );
+
+                callback();
+            }
+        );
+    }
 }
 
-MyFirstWebpackPlugin.prototype.apply = function (compiler) {
-    // 指定一个挂载到 webpack 自身的事件钩子。
-    // 设置回调来访问 compilation 对象：
-    // compilation对象 => 处理 webpack 内部实例的特定数据。
-    compiler.plugin('emit', function (compilation, callback) {
-
-        // 现在，设置回调来访问 compilation 中的步骤：
-        compilation.plugin('optimize', function () {
-            console.log('Assets are being optimized.');
-        });
-
-        // 功能完成后调用 webpack 提供的回调。
-        callback();
-    });
-};
-
-module.exports = MyFirstWebpackPlugin;
+// module.exports = MyFirstWebpackPlugin;
