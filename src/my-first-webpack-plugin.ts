8a2c49118798e39218f8cfa8453497094fe1a453
bowen-wu <13793248162@163.com>
2022-10-13 22:11:56 +0800

    feat: plugin log

---
+++ src/my-first-webpack-plugin.ts
@@ -0,0 +1,18 @@
+function MyFirstWebpackPlugin(options) {
+}
+
+MyFirstWebpackPlugin.prototype.apply = function (compiler) {
+    // 指定一个挂载到 webpack 自身的事件钩子。
+    // 设置回调来访问 compilation 对象：
+    // compilation对象 => 处理 webpack 内部实例的特定数据。
+    compiler.plugin('emit', function (compilation, callback) {
+
+        // 现在，设置回调来访问 compilation 中的步骤：
+        compilation.plugin('optimize', function () {
+            console.log('Assets are being optimized.');
+        });
+
+        // 功能完成后调用 webpack 提供的回调。
+        callback();
+    });
+};
